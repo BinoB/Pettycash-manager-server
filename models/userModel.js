708720d74e103fb77d746ae88083b534fc6fc369
BinoB <binob831@gmail.com>
2023-02-24 18:14:13 +0530

    initial commit

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+
+
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please adda Email"],
+    unique: true,
+    trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email",
+    ],
+  },
+  password:{
+    type: String,
+    required:[true,"Please add a password"],
+    minLength:[6,"Password must be up to 6 Characters"],
+/*     maxLength:[23,"Password must be up to 23 Characters"]
+ */  },
+ 
+
+
+},
+{
+    timestamps:true,
+ });
+
+
+  //encrypt password
+  userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next()
+    }
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashedPassword
+  })
+
+  
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require('path');
+
+mongoose.set("strictQuery", true);
+
+const app = express();
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pettycashmanager.app"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static(path.join(__dirname,"uploads")));
+
+//Routes middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+//Error Middleware
+app.use(errorHandler);
+
+//Database connection
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server running on port${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

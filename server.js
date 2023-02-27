776cd8ad524563006299780639e8399150ccd7ed
BinoB <binob831@gmail.com>
2023-02-27 23:52:10 +0530

    latest change 10

--- server.js
+++ server.js
@@ -28,12 +28,7 @@ app.use(cors({
 }));
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pettycash-manager.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 
 

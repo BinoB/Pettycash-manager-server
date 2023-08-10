ef0c37c46b595ee387c69fa4be88dbdf46e26e85
BinoB <binob831@gmail.com>
2023-08-10 17:39:30 +0530

    modigied

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pettycash-manager.vercel.app"],
+    origin: ["http://localhost:3000", "https://pettycash-manager.vercel.app","https://main--adorable-tanuki-92cddb.netlify.app"],
     credentials: true,
   })
   //

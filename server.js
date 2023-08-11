9d24e18799b8b5fcad673b13e0c5d14d25837090
BinoB <binob831@gmail.com>
2023-08-11 12:21:28 +0530

    error

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 });
 
 // Error Middleware
-app.use(errorHandler);
+/* app.use(errorHandler); */
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
